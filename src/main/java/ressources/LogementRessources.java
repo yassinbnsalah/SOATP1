2c08535925ff64f9e4c19e9c33a16b0d5e541d8d
yassinebnsalah <yacinbnsalh@gmail.com>
2023-10-27 16:50:25 +0100

    FIX

--- src/main/java/ressources/LogementRessources.java
+++ src/main/java/ressources/LogementRessources.java
@@ -15,17 +15,51 @@ import java.util.List;
 @Path("logements")
 public class LogementRessources {
     public static LogementBusiness logementMetier = new LogementBusiness();
-
-@POST
-@Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("add")
     public Response addLogement(Logement l) {
-     if(logementMetier.addLogement(l))
-         return  Response.status(Status.CREATED).build();
-     return  Response.status(Status.NOT_FOUND).build();
+        if(logementMetier.addLogement(l))
+            return  Response.status(Status.CREATED).build();
+        return  Response.status(Status.NOT_FOUND).build();
     }
 
-    @Secured
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findbygouvernat(@QueryParam(value ="reference") Integer d)
+    {
+        if(d != null)
+        {
+            if(logementMetier.getLogementsByReference(d)!=null) {
+                Logement l=new Logement();
+                l=logementMetier.getLogementsByReference(d);
+                return Response.status(Status.OK).entity(l).build();
+            }
+            return Response.status(Status.NOT_FOUND).entity("impossible de trouver").build();
+        }
+        return Response.status(Status.OK).entity(logementMetier.getLogements()).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{reference}")
+    public Response findbygouvernatpa(@PathParam(value ="reference") Integer d)
+    {
+        if(d != null)
+        {
+            if(logementMetier.getLogementsByReference(d)!=null) {
+                Logement l=new Logement();
+                l=logementMetier.getLogementsByReference(d);
+                return Response.status(Status.OK).entity(l).build();
+            }
+            return Response.status(Status.NOT_FOUND).entity("impossible de trouver").build();
+        }
+        return Response.status(Status.OK).entity(logementMetier.getLogements()).build();
+    }
+
+    //@Secured
 @GET
+@Path("all")
 @Produces(MediaType.APPLICATION_JSON)
     public Response getLogements() {
         List<Logement> liste=logementMetier.getLogements();
@@ -33,12 +67,24 @@ public class LogementRessources {
         return  Response.status(Status.NOT_FOUND).build();
     return  Response.status(Status.OK).entity(liste).build();
 
-
     }
+    @GET
+    @Path("get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response  GetAll()
+    {
 
-/*@PUT
-@Consumes(MediaType.APPLICATION_JSON)
-    public Response updateLogement(Logement updatedLogement, int reference) {
+        if(logementMetier.getLogements()!=null)
+            return Response.status(Status.OK).entity(logementMetier.getLogements()).build();
+
+        else
+
+            return Response.status(Status.NOT_FOUND).entity("liste vide" ).build();
+    }
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{reference}")
+    public Response updateLogement(Logement updatedLogement, @PathParam(value ="reference") int reference) {
 
 
         if (logementMetier.updateLogement(reference,updatedLogement)) {
@@ -46,10 +92,11 @@ public class LogementRessources {
         } else {
             return Response.status(Status.NOT_FOUND).build();
         }
-    }*/
+    }
 
-@DELETE
-        public  Response deleteLogement(int reference){
+    @DELETE
+    @Path("{reference}")
+        public  Response deleteLogement( @PathParam(value ="reference")int reference){
            if(logementMetier.deleteLogement(reference))
                     return Response.status(Status.OK).build();
 
--- src/main/java/ressources/RendezVousRessources.java
+++ src/main/java/ressources/RendezVousRessources.java
@@ -23,10 +23,10 @@ public class RendezVousRessources {
             return  Response.status(Response.Status.CREATED).build();
         return  Response.status(Response.Status.NOT_ACCEPTABLE).build();
     }
-    @Secured
+    //@Secured
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getRendezVous(String refLogement) {
+    public Response getRendezVous(@QueryParam("refLogement") String refLogement) {
         List<RendezVous> liste=new ArrayList<RendezVous>();
         if(refLogement != null) {
             liste = rendezVousMetier.getListeRendezVousByLogementReference(Integer.parseInt(refLogement));
@@ -42,6 +42,7 @@ public class RendezVousRessources {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{id}")
     public Response updateRdv(RendezVous updatedRendezVous, @PathParam("id") int id) {
 
 
@@ -53,7 +54,8 @@ public class RendezVousRessources {
     }
 
     @DELETE
-    public  Response deleteRendezVous(int id){
+    @Path("{id}")
+    public  Response deleteRendezVous(@PathParam("id") int id){
         if(rendezVousMetier.deleteRendezVous(id))
             return Response.status(Response.Status.OK).build();
 
@@ -63,7 +65,8 @@ public class RendezVousRessources {
     }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public  Response getRendezVousbyId(int id){
+    @Path("{id}")
+    public  Response getRendezVousbyId(@PathParam("id") int id){
         if(rendezVousMetier.getRendezVousById(id)!=null)
             return Response.status(Response.Status.OK).entity(rendezVousMetier.getRendezVousById(id)).build();
 
